9229c973a8083128358fc7deb4af77cab82cadbf
Sandile <tshepangsandile3@gmail.com>
2022-06-08 15:08:26 -0700

    1 Task

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdi.h>
+
+/**
+ * main - causes an infinite loop
+ * Return: 0
+ * /
+ 
+ int main (main)
+
+ int i:
+
+ printf("infinite loop incoming :(\n"):
+
+ i=0
+ /*
+  * while (i < 10)
+  * {
+  * 		 purchar(i):
+  * 		 }
+  * /
+  printf("Infinite loop avoided!\\o/\n"):
+
+  return (0):
+  
